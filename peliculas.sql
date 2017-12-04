0f044be0351d92b75865f77049b6fa758079f162
savANDY <andicuu@gmail.com>
2017-12-04 12:35:54 +0100

    Actualizada BBDD

--- peliculas.sql
+++ peliculas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-11-2017 a las 09:23:05
+-- Tiempo de generación: 04-12-2017 a las 12:35:36
 -- Versión del servidor: 10.1.26-MariaDB
 -- Versión de PHP: 7.1.8
 
@@ -42,6 +42,20 @@ RIGHT JOIN actores a ON ac.idActor=a.idActor
 GROUP BY a.idActor;
 END$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `actores_cuantas_peliculas` ()  NO SQL
+BEGIN
+SELECT a.idActor, a.Nombre, COUNT(DISTINCT p.Titulo) cant_pelis
+FROM peliculas p INNER JOIN actuaciones ac ON ac.idPelicula=p.idPelicula 
+RIGHT JOIN actores a ON ac.idActor=a.idActor 
+GROUP BY a.idActor;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `borrar_pelicula` (IN `p_idPelicula` INT)  NO SQL
+BEGIN
+SELECT titulo FROM peliculas where idPelicula = p_idPelicula;
+DELETE FROM peliculas WHERE idPelicula = p_idPelicula;
+END$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insertar_actor` (IN `p_nombre` VARCHAR(40))  NO SQL
 INSERT INTO actores(nombre) VALUES (p_nombre)$$
 
@@ -62,17 +76,30 @@ END IF;
 
 END$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertar_actuacion` (IN `p_idActor` INT, IN `p_esProtagonista` BOOLEAN)  NO SQL
+BEGIN
+DECLARE sel_idPelicula int;
+set sel_idPelicula = (SELECT MAX(idPelicula) from peliculas);
+
+INSERT INTO actuaciones(idActor, idPelicula, esProtagonista) VALUES 			(p_idActor, sel_idPelicula, p_esProtagonista);
+
+END$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insertar_director` (IN `p_nombre` VARCHAR(40))  NO SQL
 INSERT INTO directores(Nombre) VALUES (p_nombre)$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insertar_pelicula` (IN `p_titulo` VARCHAR(40), IN `p_anyo` YEAR(4), IN `p_director` INT, IN `p_cartel` VARCHAR(200))  NO SQL
 BEGIN
 
-IF EXISTS (SELECT idDirector FROM directores WHERE idDirector = p_director)THEN
-        INSERT INTO peliculas(titulo, anyo, idDirector, cartel) VALUES 			(p_nombre, p_anyo, p_director, p_cartel);
-ELSE 
-    SELECT 'No existe ese director';
-END IF;
+INSERT INTO peliculas(titulo, anyo, idDirector, cartel) VALUES 			(p_titulo, p_anyo, p_director, p_cartel);
+
+
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `modificar_pelicula` (IN `p_idPelicula` INT, IN `p_titulo` VARCHAR(40), IN `p_anyo` YEAR(4), IN `p_director` INT, IN `p_cartel` VARCHAR(200))  NO SQL
+BEGIN
+
+UPDATE `peliculas` SET `titulo`=p_titulo,`anyo`=p_anyo,`idDirector`=p_director,`cartel`=p_cartel  WHERE `idPelicula`= p_idPelicula;
 
 END$$
 
@@ -95,7 +122,7 @@ END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `peliculas_con_director` ()  NO SQL
 BEGIN
-SELECT p.idPelicula, p.titulo, p.anyo Año, d.Nombre Director
+SELECT p.idPelicula, p.titulo, p.anyo Año, d.Nombre Director, p.cartel
 FROM peliculas p, directores d
 WHERE p.idDirector = d.idDirector
 ;
@@ -124,6 +151,18 @@ END$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `peliculas_posteriores_2000` ()  NO SQL
 BEGIN
 SELECT * FROM peliculas WHERE anyo > 2000;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `pelicula_por_id` (IN `p_idPelicula` INT)  NO SQL
+BEGIN
+select p.idPelicula, p.titulo, p.anyo Año, p.idDirector, d.Nombre Director, p.cartel, GROUP_CONCAT(a.nombre separator '<br>') Actores
+from peliculas p
+
+left join actuaciones act on act.idPelicula=p.idPelicula
+left join actores a on act.idActor=a.idActor
+left join directores d on p.idDirector = d.idDirector
+WHERE (p.idPelicula = p_idPelicula)
+GROUP BY p.titulo;
 END$$
 
 DELIMITER ;
@@ -171,19 +210,15 @@ CREATE TABLE `actuaciones` (
 
 INSERT INTO `actuaciones` (`idActor`, `idPelicula`, `esProtagonista`) VALUES
 (1, 1, 0),
-(1, 4, 0),
-(1, 10, 0),
+(1, 26, 1),
 (2, 1, 0),
-(2, 3, 0),
-(2, 4, 0),
+(2, 26, 0),
 (3, 1, 1),
+(3, 26, 0),
 (4, 1, 0),
-(4, 3, 1),
 (5, 1, 0),
 (6, 1, 0),
-(6, 3, 0),
-(7, 1, 0),
-(7, 3, 1);
+(7, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -227,24 +262,17 @@ CREATE TABLE `peliculas` (
 --
 
 INSERT INTO `peliculas` (`idPelicula`, `titulo`, `anyo`, `idDirector`, `cartel`) VALUES
-(1, 'Los 16 pesados de clase', 2016, 5, ''),
-(2, 'El negro y la clase', 2017, 3, ''),
-(3, 'Invierno frio', 2011, 4, ''),
-(4, 'Primavera Rojiza', 2015, 2, ''),
-(5, 'Verano Ardiente', 2013, 1, ''),
-(6, 'Verano Caliente', 1999, 5, ''),
-(7, 'Otoño Salado', 1998, 5, ''),
-(8, 'Fast And Furious 1', 1995, 3, ''),
-(9, 'Fast And Furious 2', 2000, 3, ''),
-(10, 'Fast And Furious 3', 2002, 1, ''),
-(11, 'Fast And Furious 4', 2004, 1, ''),
-(12, 'Fast And Furious 5', 2006, 4, ''),
-(13, 'Fast And Furious 6', 2008, 4, ''),
-(14, 'Fast And Furious 7', 2010, 2, ''),
-(15, 'Fast And Furious 8', 2015, 2, ''),
-(16, 'Bordeaux Falling', 2016, 6, ''),
-(17, 'Paris Driving', 2017, 6, ''),
-(18, 'Toy Story', 2017, 1, '');
+(1, 'Los 16 pesados de clase', 2016, 5, 'http://es.web.img2.acsta.net/r_1280_720/pictures/210/127/21012757_20130616001705563.jpg'),
+(16, 'Feliz día de tu muerte', 2016, 6, 'https://demoalto-impresionesaerea.netdna-ssl.com/2017/10/Feliz-dia-de-tu-muerte-poster.jpg'),
+(17, 'Paddington 2', 2017, 6, 'https://filasiete.com/wp-content/uploads/2017/11/95369.jpg'),
+(18, 'Toy Story', 2017, 1, 'https://1.bp.blogspot.com/-4CEJ24flM5U/T-ePLTwLdyI/AAAAAAAAHcQ/GGYVN8SVxG0/s275/toy-story-1995.jpg'),
+(20, 'Deep', 2017, 1, 'https://ccbuenavistasantamarta.com/wp-content/uploads/2017/11/deep.jpg.pagespeed.ce.IfUqqVN6NK.jpg'),
+(21, 'PALMERAS EN LA NIEVE', 2015, 3, 'https://imagessl8.casadellibro.com/a/l/t0/38/9788499985138.jpg'),
+(22, '', 2009, 1, 'http://4.bp.blogspot.com/-AW6fFVemtro/UcIVsXqah1I/AAAAAAAAABY/pikV48uCK3k/s1600/la-huerfana_portada.jpg'),
+(23, 'YO ANTES DE TI', 2014, 5, 'http://www.canalfreak.net/wp-content/uploads/2016/05/531x803xunnamed-3_phixr.jpg.pagespeed.ic.JRI_dUMmec.jpg'),
+(24, 'OBLIVION', 2013, 4, 'https://revientaguiones.files.wordpress.com/2013/07/caratula-portada-de-la-pelicula-oblivion-de-tom-cruise.jpg'),
+(25, 'Intocable', 2011, 6, 'http://todossomosuno.com.mx/portal/wp-content/uploads/2013/08/portada-intocable.jpg'),
+(26, 'Titanic', 1996, 2, 'http://especiales.eluniverso.com/titanic/wp-content/uploads/2012/04/Titanic-pelicula-de-television-1996.jpg');
 
 -- --------------------------------------------------------
 
@@ -364,7 +392,7 @@ ALTER TABLE `directores`
 -- AUTO_INCREMENT de la tabla `peliculas`
 --
 ALTER TABLE `peliculas`
-  MODIFY `idPelicula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `idPelicula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
